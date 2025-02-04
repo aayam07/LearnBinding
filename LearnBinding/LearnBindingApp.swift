cf1180340ba3015bc9ee57079c0df59a4b041134
Aayam Adhikari <thisisleo7@gmail.com>
2025-02-04 11:32:35 +0545

    Initial Commit

---
+++ LearnBinding/LearnBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnBindingApp.swift
+//  LearnBinding
+//
+//  Created by Aayam Adhikari on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnBindingTests/LearnBindingTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnBindingTests.swift
+//  LearnBindingTests
+//
+//  Created by Aayam Adhikari on 04/02/2025.
+//
+
+import Testing
+@testable import LearnBinding
+
+struct LearnBindingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
