5da0809afd3664e6ef0f74baf1cb20bc1fee954e
Aayam Adhikari <thisisleo7@gmail.com>
2025-02-04 13:33:50 +0545

    Implement @EnvironmentObject to make a global state varible to be accessible by all nested views.

--- LearnBinding/ContentView.swift
+++ LearnBinding/ContentView.swift
@@ -11,21 +11,38 @@ import SwiftUI
 // That means, a @Binding variable is always created in the child view. The parent view passes one of its variables to the child view as a binding so that the communication is possible between them.
 
 
+// Environment Object is basically a global state variable
+
+
+// Pre iOS 17
+class AppState: ObservableObject {
+    @Published var isOn: Bool = false
+}
+
+
 // child view
 struct LightBulbView: View {
     
-    @Binding var isOn: Bool
+    @EnvironmentObject private var appState: AppState
     
     var body: some View {
         VStack {
-            Image(systemName: isOn ? "lightbulb.fill" : "lightbulb")
+            Image(systemName: appState.isOn ? "lightbulb.fill" : "lightbulb")
                 .font(.largeTitle)
-                .foregroundStyle(isOn ? .yellow : .black)
+                .foregroundStyle(appState.isOn ? .yellow : .black)
             
             Button("Toggle") {
-                isOn.toggle()
+                appState.isOn.toggle()
             }
         }
+    }
+}
+
+
+struct LightRoomView: View {
+    
+    var body: some View {
+        LightBulbView()
     }
 }
 
@@ -33,18 +50,19 @@ struct LightBulbView: View {
 // parent view: as it uses the child view
 struct ContentView: View {
     
-    @State private var isLightOn: Bool = false
+    @EnvironmentObject var appState: AppState
     
     var body: some View {
         VStack {
-            LightBulbView(isOn: $isLightOn)
+            LightRoomView()
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(isLightOn ? .gray : .green)
+        .background(appState.isOn ? .gray : .white)
     }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(AppState())
 }
