46610e236b62da0040a9ddf775785f0fe3a53add
Aayam Adhikari <thisisleo7@gmail.com>
2025-02-04 11:55:17 +0545

    Implement Binding varibale in child view and pass it from parent view

--- LearnBinding/ContentView.swift
+++ LearnBinding/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
+// Binding is always passed from the parent view to the child view
+// That means, a @Binding variable is always created in the child view. The parent view passes one of its variables to the child view as a binding so that the communication is possible between them.
+
+
+// child view
+struct LightBulbView: View {
+    
+    @Binding var isOn: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: isOn ? "lightbulb.fill" : "lightbulb")
+                .font(.largeTitle)
+                .foregroundStyle(isOn ? .yellow : .black)
+            
+            Button("Toggle") {
+                isOn.toggle()
+            }
+        }
+    }
+}
+
+
+// parent view: as it uses the child view
+struct ContentView: View {
+    
+    @State private var isLightOn: Bool = false
+    
+    var body: some View {
+        VStack {
+            LightBulbView(isOn: $isLightOn)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(isLightOn ? .gray : .green)
     }
 }
 
